aa012f7ef21d0289938dc906a77a81b36c5cd54f
Frank Hollywood <github@srvsync.com>
2021-08-17 13:38:54 -0700

    Created HTML and started JS

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // import functions and grab DOM elements
+const userGuess = document.getElementById('inputNumber');
+const guessButton = document.getElementById('submitGuess');
+const guessEl = document.getElementById('highLow');
+const numTries = document.getElementById('numberOfTries');
+const resultsEl = document.getElementById('results');
 
+//console.log(userGuess, guessButton, guessEl, numTries, resultsEl);
+let counter1 = 0;
+let counter2 = 0;
 // initialize global state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
